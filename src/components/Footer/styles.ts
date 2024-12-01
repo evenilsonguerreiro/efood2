6eff98bb87f7c48c88c3e8ed08661f4513c23944
evenilson <evenilsonguerreiro@gmail.com>
2024-12-01 19:13:22 -0300

    correção de formatação

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components"
-import { cores } from "../../styles"
+import styled from 'styled-components'
+import { cores } from '../../styles'
 
 export const FooterBar = styled.div`
   position: relative;
