67f095a1a1541b40274911a7286a7c02a6087eb4
evenilson <evenilsonguerreiro@gmail.com>
2024-12-14 15:57:10 -0300

    ajuste product2

--- src/components/Product2/styles.ts
+++ src/components/Product2/styles.ts
@@ -4,7 +4,7 @@ import { cores } from '../../styles'
 export const Card2 = styled.div`
   position: relative;
   width: 320px;
-  height: 338px;
+  height: 348px;
   margin-top: 40px;
   background-color: ${cores.pink};
 
@@ -22,6 +22,7 @@ export const CardImg = styled.div`
     top: 10px;
     left: 8px;
     width: 300px;
+    height: 160px;
   }
 
   @media (max-width: 768px) {
@@ -32,25 +33,25 @@ export const CardImg = styled.div`
   }
 `
 export const CardTitle2 = styled.h3`
-  width: 124px;
+  width: 159px;
   height: 19px;
   font-size: 16px;
   font-weight: 900;
-  line-height: 18.75px;
+  line-height: 18px;
   color: ${cores.branco};
   position: absolute;
-  top: 190px;
+  top: 178px;
   left: 8px;
 `
 export const CardDescricao2 = styled.p`
   width: 304px;
   height: 88px;
   color: ${cores.branco};
-  font-size: 14px;
-  line-height: 22px;
+  font-size: 12px;
+  line-height: 20px;
   font-weight: 400;
   position: absolute;
-  top: 210px;
+  top: 213px;
   left: 8px;
 `
 export const CardButton = styled.button`
@@ -64,7 +65,7 @@ export const CardButton = styled.button`
   line-height: 16.41px;
   border: none;
   position: absolute;
-  top: 305px;
+  top: 316px;
   left: 8px;
   cursor: pointer;
 
