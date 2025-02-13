f72a3a2f5a9a4bfe6048702f8fd0d8c6fbdeced0
evenilson <evenilsonguerreiro@gmail.com>
2025-02-13 15:20:33 -0300

    ajs

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -9,6 +9,7 @@ export const Lista = styled.div`
   justify-content: center;
   gap: 16px;
 `
+
 export const Card = styled.div`
   background-color: ${cores.branco2};
   width: 472px;
@@ -23,24 +24,29 @@ export const Card = styled.div`
     object-fit: cover;
   }
 `
+
 export const Fundo = styled.div`
   padding: 5px;
 `
+
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: 700;
   color: ${cores.pink};
 `
+
 export const Descricao = styled.p`
   font-size: 14px;
   font-weight: 400;
   line-height: 22px;
   color: ${cores.pink};
 `
+
 export const Infos = styled.div`
   position: absolute;
-  top: 10px; /* Ajuste conforme necessário */
-  left: 300px;
+  top: 10px;
+  left: 85%; /* Centraliza horizontalmente */
+  transform: translateX(-50%); /* Ajusta para centralização exata */
   padding: 5px 10px;
   border-radius: 5px;
   display: flex;
