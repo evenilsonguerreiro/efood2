b1cc33171d76ee92575889428dd9ebd6bc344f32
evenilson <evenilsonguerreiro@gmail.com>
2025-02-17 14:21:38 -0300

    container

--- src/styles.ts
+++ src/styles.ts
@@ -40,5 +40,8 @@ export const EstiloGlobal = createGlobalStyle`
     width: 100%;
     max-width: 1440px;
     margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
   }
 `
