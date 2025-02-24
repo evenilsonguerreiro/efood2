ca0db11c0034ebeaaceafeb27577b5fffbaaaa91
evenilson <evenilsonguerreiro@gmail.com>
2025-02-24 11:38:02 -0300

    alteracao 3

--- src/components/ProductList/styles.ts
+++ src/components/ProductList/styles.ts
@@ -3,11 +3,17 @@ import { Props } from './index'
 
 export const Container = styled.section<Omit<Props, 'title' | 'comidas'>>`
   background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  padding: 20px;
 `
 export const List = styled.ul`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(2, 1fr); /* Força 2 colunas */
   gap: 24px;
-  width: 1024px;
   width: 100%;
+  max-width: 1024px; /* Limita a largura total do grid */
+  margin: 0 auto;
 `
