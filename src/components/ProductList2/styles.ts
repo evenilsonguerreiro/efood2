bb9f2629e2e04900a1776229c5d6b05dcdf7abdd
evenilson <evenilsonguerreiro@gmail.com>
2025-02-26 10:17:38 -0300

    alteracao 8

--- src/components/ProductList2/styles.ts
+++ src/components/ProductList2/styles.ts
@@ -10,4 +10,13 @@ export const List2 = styled.ul`
   grid-template-columns: 1fr 1fr 1fr;
   gap: 24px;
   margin: 0 auto;
+
+  @media screen and (min-width: 1920px) and (min-height: 1080px) {
+    .Lista2 {
+      top: -50%;
+      left: -50%;
+      transform: translate(50%, 50%);
+      width: auto;
+    }
+  }
 `
